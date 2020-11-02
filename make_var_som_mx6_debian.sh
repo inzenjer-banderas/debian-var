8ab9d0a7e748254ffaae5a1f6a9aaee5fa823ab0
Lazar Sladojevic <lazar.neg@hotmail.com>
2020-11-02 15:39:55 +0100

    Changed dts filename

--- make_var_som_mx6_debian.sh
+++ make_var_som_mx6_debian.sh
@@ -46,7 +46,7 @@ readonly G_LINUX_KERNEL_REV="930b72fd05ac3f6c199ddd8cb9d24ac2a8296883"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 #readonly G_LINUX_DTB='imx6dl-var-som-cap.dtb imx6dl-var-som-res.dtb imx6dl-var-som-solo-cap.dtb imx6dl-var-som-solo-res.dtb imx6dl-var-som-solo-vsc.dtb imx6dl-var-som-vsc.dtb imx6q-var-dart.dtb imx6q-var-som-cap.dtb imx6q-var-som-res.dtb imx6q-var-som-vsc.dtb imx6qp-var-som-cap.dtb imx6qp-var-som-res.dtb imx6qp-var-som-vsc.dtb'
 
-readonly G_LINUX_DTB='imx6q-var-som-res.dtb'
+readonly G_LINUX_DTB='nano_edge_sound_recorder.dts'
 
 ## uboot
 readonly G_UBOOT_SRC_DIR="${DEF_SRC_DIR}/uboot"
